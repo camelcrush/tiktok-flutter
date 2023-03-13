b098d32331d4058eb8ea26acf3bf3d4457f4882d
camelcrush <dkxm0927@naver.com>
2023-03-13 23:23:58 +0900

    Email Screen

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktokapp/features/authentication/widgets/form_button.dart';
-
+import 'package:tiktokapp/features/password_screen.dart';
 import '../../constants/gaps.dart';
 import '../../constants/size.dart';
 
@@ -8,22 +8,22 @@ class EmailScreen extends StatefulWidget {
   const EmailScreen({Key? key}) : super(key: key);
 
   @override
-  State<EmailScreen> createState() => _UsernameScreenState();
+  State<EmailScreen> createState() => _EmailScreenState();
 }
 
-class _UsernameScreenState extends State<EmailScreen> {
+class _EmailScreenState extends State<EmailScreen> {
   // TextEitingController : TextField() 컨트롤러 등록을 위한 변수 선언
-  final TextEditingController _usernameController = TextEditingController();
-  String _username = "";
+  final TextEditingController _emailController = TextEditingController();
+  String _email = "";
 
   @override
   void initState() {
     super.initState();
     // TextField() Controller EventListener
-    _usernameController.addListener(() {
-      // _username Sate값 업데이트
+    _emailController.addListener(() {
+      // _email Sate값 업데이트
       setState(() {
-        _username = _usernameController.text;
+        _email = _emailController.text;
       });
     });
   }
@@ -31,53 +31,95 @@ class _UsernameScreenState extends State<EmailScreen> {
 // 메모리를 위해 꼭 사용한 위젯 Controller dispose해주기
   @override
   void dispose() {
-    _usernameController.dispose();
+    _emailController.dispose();
     super.dispose();
+  }
+
+// Email Validation Check
+  String? _isEmailValid() {
+    if (_email.isEmpty) return null;
+    // Email 형식 정규표현식
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!regExp.hasMatch(_email)) {
+      return 'Email not valid.';
+    }
+    return null;
+  }
+
+// FocusScope : keyboard avoid 기능
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+// onSubmit
+  void _onSubmit() {
+    if (_email.isEmpty || _isEmailValid() != null) return;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const PasswordScreen(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size36,
+    // Keyboard Avoiding을 위해 GestureDetector 위젯 사용
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sign Up'),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v40,
-            const Text(
-              'What is your email?',
-              style: TextStyle(
-                fontSize: Sizes.size24,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Gaps.v16,
-            TextField(
-              // Controller 등록
-              controller: _usernameController,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey.shade400,
-                  ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey.shade400,
-                  ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                'What is your email?',
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
-              cursorColor: Theme.of(context).primaryColor,
-            ),
-            Gaps.v28,
-            FormButton(disabled: _username.isEmpty),
-          ],
+              Gaps.v16,
+              TextField(
+                onEditingComplete: _onSubmit,
+                keyboardType: TextInputType.emailAddress,
+                autocorrect: false,
+                // Controller 등록
+                controller: _emailController,
+                decoration: InputDecoration(
+                  // errorText
+                  errorText: _isEmailValid(),
+                  hintText: 'Email',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onSubmit,
+                child: FormButton(
+                  disabled: _email.isEmpty || _isEmailValid() != null,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
