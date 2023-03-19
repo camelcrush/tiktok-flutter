48202ba681235954d1511c1899f79b9960c1b567
camelcrush <dkxm0927@naver.com>
2023-03-19 15:26:38 +0900

    NavigationBar

--- lib/features/main_navigation/main_navigation.dart
+++ lib/features/main_navigation/main_navigation.dart
@@ -38,36 +38,27 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        // BottomNavigationBarType.shifting : Item이 4개 이상일 때 자동 적용되는 Tap효과를 강제적으로 적용
-        type: BottomNavigationBarType.shifting,
-        selectedItemColor: Theme.of(context).primaryColor,
-        currentIndex: _selectedIndex,
-        onTap: _onTap,
-        items: const [
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.house),
-            label: 'Home',
-            tooltip: "What are you?",
-            backgroundColor: Colors.amber,
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-            label: 'Search',
-            tooltip: "What are you?",
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(
-              FontAwesomeIcons.house,
+        body: screens[_selectedIndex],
+        bottomNavigationBar: NavigationBar(
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: _onTap,
+          destinations: const [
+            NavigationDestination(
+              icon: FaIcon(
+                FontAwesomeIcons.house,
+                color: Colors.black87,
+              ),
+              label: 'Home',
             ),
-            label: 'Home',
-            tooltip: "What are you?",
-            backgroundColor: Colors.teal,
-          ),
-        ],
-      ),
-    );
+            NavigationDestination(
+              icon: FaIcon(
+                FontAwesomeIcons.magnifyingGlass,
+                color: Colors.black87,
+              ),
+              label: 'Search',
+            )
+          ],
+        ));
   }
 }
