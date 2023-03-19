65e47c5a3320f8f07a2c2e64675559687a601895
camelcrush <dkxm0927@naver.com>
2023-03-19 17:28:48 +0900

    Custom NavigationBar

--- lib/features/main_navigation/main_navigation.dart
+++ lib/features/main_navigation/main_navigation.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktokapp/constants/size.dart';
+import 'package:tiktokapp/features/main_navigation/widgests/tab_nav.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({Key? key}) : super(key: key);
@@ -8,42 +11,68 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
   final screens = [
     const Center(
-      child: Text('Home'),
+      child: Text(
+        'Home',
+        style: TextStyle(fontSize: 49),
+      ),
     ),
     const Center(
-      child: Text('Search'),
+      child: Text(
+        'Search',
+        style: TextStyle(fontSize: 49),
+      ),
     ),
-    const Center(
-      child: Text('Home'),
-    ),
-    const Center(
-      child: Text('Search'),
-    ),
-    const Center(
-      child: Text('Search'),
-    )
   ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // CupertinoTabScaffold를 사용하려면 main.dart에서 CupertinoApp으로 전환해야 함
-    return CupertinoTabScaffold(
-      // CupertinoTabBar는 별도로 index state를 설정할 필요가 없음
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.house),
-            label: 'Home',
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TabNav(
+                text: 'Home',
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              TabNav(
+                text: 'Discover',
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                onTap: () => _onTap(1),
+              ),
+              TabNav(
+                text: 'Inbox',
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                onTap: () => _onTap(3),
+              ),
+              TabNav(
+                text: 'Profile',
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                onTap: () => _onTap(4),
+              )
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.search),
-            label: 'Search',
-          )
-        ],
+        ),
       ),
-      tabBuilder: (context, index) => screens[index],
     );
   }
 }
---
+++ lib/features/main_navigation/widgests/tab_nav.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktokapp/constants/gaps.dart';
+
+class TabNav extends StatelessWidget {
+  const TabNav({
+    Key? key,
+    required this.text,
+    required this.isSelected,
+    required this.icon,
+    required this.onTap,
+  }) : super(key: key);
+
+  final String text;
+  final bool isSelected;
+  final IconData icon;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    // Expanded : Row나 Column등에서 핸드폰 화면에 맞게 균일하게 배치하기 위해서 사용함.
+    // 부모의 남은 범위를 flex의 비율에 맞춰서 "모두" 가져간다. 여기서는 MainNavigation의 Row
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          // Container에 color값을 주어야 자체적으로 공간을 Expanded함
+          color: Colors.black,
+          child: AnimatedOpacity(
+            opacity: isSelected ? 1 : 0.3,
+            duration: const Duration(milliseconds: 300),
+            child: Column(
+              // Column은 기본적으로 화면을 전부 사용할려고 하기 때문에 MainAxisSize.min으로 자식 사이즈에 맞춤
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(
+                  icon,
+                  color: Colors.white,
+                ),
+                Gaps.v5,
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
