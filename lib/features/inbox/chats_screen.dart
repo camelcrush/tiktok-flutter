22024fe5675ce72b784e3af65f4ed3d036f9fee3
camelcrush <dkxm0927@naver.com>
2023-03-30 22:53:34 +0900

    AnimatedList: GlobalKey<AnimatedListState>

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -10,6 +10,24 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  // StatefulWidget에서 각 위젯들을 구분짓기 위해서는 key가 필요하고 여기서는 GlobalKey를 설정
+  // Key를 통해 해당 위젯의 state에 접근할 수 있음
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    // AnimatedList에서 initialItemCount = 0으로 초기 state값이 설정되어 있는데
+    // key값을 통해 해당 위젯의 state값(ItemCount)을 증가시킴
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(milliseconds: 200),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,41 +36,50 @@ class _ChatsScreenState extends State<ChatsScreen> {
         title: const Text("Direct Message"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(FontAwesomeIcons.plus),
           )
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 25,
-              foregroundImage: NetworkImage(
-                  "https://lh3.googleusercontent.com/a/AGNmyxYol5lNtQShTuXHxFwtUaHFG7SJ7NgONKeSCEz9jg=s96-c-rg-br100"),
-              child: Text("JS"),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  "Camel",
-                  style: TextStyle(fontWeight: FontWeight.bold),
+        // itemBuilder를 통해 제공되는 context, index, animation
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 25,
+                  foregroundImage: NetworkImage(
+                      "https://lh3.googleusercontent.com/a/AGNmyxYol5lNtQShTuXHxFwtUaHFG7SJ7NgONKeSCEz9jg=s96-c-rg-br100"),
+                  child: Text("JS"),
                 ),
-                Text(
-                  "2:16 PM",
-                  style: TextStyle(
-                    color: Colors.grey.shade600,
-                    fontSize: Sizes.size12,
-                  ),
-                )
-              ],
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      "Camel",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "2:16 PM",
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontSize: Sizes.size12,
+                      ),
+                    )
+                  ],
+                ),
+                subtitle: const Text("Don't forget to make video"),
+              ),
             ),
-            subtitle: const Text("Don't forget to make video"),
-          )
-        ],
+          );
+        },
       ),
     );
   }
