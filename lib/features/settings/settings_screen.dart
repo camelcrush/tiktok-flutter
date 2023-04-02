e4cae3a2d723735b2305f1f5953689db61905318
camelcrush <dkxm0927@naver.com>
2023-04-02 11:24:44 +0900

    CupertinoAlertDialog, AlertDialog

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -41,6 +43,50 @@ class _SettingsScreenState extends State<SettingsScreen> {
             onChanged: _onNotoficationsChanged,
             title: const Text("Enable Notifications"),
             subtitle: const Text("Enable Notifications"),
+          ),
+          ListTile(
+            title: const Text("Log out (IOS)"),
+            textColor: Colors.red,
+            onTap: () => showCupertinoDialog(
+              context: context,
+              builder: (context) => CupertinoAlertDialog(
+                title: const Text("Are you sure?"),
+                content: const Text("Plz don't go"),
+                actions: [
+                  CupertinoDialogAction(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text("NO"),
+                  ),
+                  CupertinoDialogAction(
+                    onPressed: () => Navigator.of(context).pop(),
+                    isDestructiveAction: true,
+                    child: const Text("Yes"),
+                  )
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            title: const Text("Log out (Android)"),
+            textColor: Colors.red,
+            onTap: () => showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                icon: const FaIcon(FontAwesomeIcons.skull),
+                title: const Text("Are you sure?"),
+                content: const Text("Plz don't go"),
+                actions: [
+                  IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: const FaIcon(FontAwesomeIcons.xmark),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text("Yes"),
+                  )
+                ],
+              ),
+            ),
           ),
           ListTile(
             onTap: () async {
