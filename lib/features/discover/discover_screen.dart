69a6d9ca5586ec7ff866e2367507f655e63c05d8
camelcrush <dkxm0927@naver.com>
2023-04-04 23:17:43 +0900

    MediaQuery, LayoutBuilder, ConstrainedBox for Responsive Design

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktokapp/constants/breakpoints.dart';
 import 'package:tiktokapp/constants/gaps.dart';
 import 'package:tiktokapp/constants/size.dart';
 
@@ -61,22 +62,32 @@ class _DiscoverScreenState extends State<DiscoverScreen>
 
   @override
   Widget build(BuildContext context) {
-    // TabBar 혹은 TabBarView를 사용하려면 Controller가 필요
-    // DefaultTabController를 사용
+    // Widget of the week.
+    // https://www.youtube.com/watch?v=A3WrA4zAaPw
+    final width = MediaQuery.of(context).size.width;
+
     return GestureDetector(
       onTap: _onScaffoldTap,
+      // TabBar 혹은 TabBarView를 사용하려면 Controller가 필요
+      // DefaultTabController를 사용
       child: DefaultTabController(
         length: tabs.length,
         child: Scaffold(
           resizeToAvoidBottomInset: false,
           appBar: AppBar(
             elevation: 1,
-            // CupertinoSearchTextField
-            title: CupertinoSearchTextField(
-              controller: _textEditingController,
-              onChanged: _onSearchChanged,
-              onSubmitted: _onSearchSubmitted,
-              autocorrect: false,
+            // ConstrainedBox : 크기의 제한값을 걸어주는 Container, 일반 Container에서도 constraints값을 받음
+            title: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: Breakpoints.sm,
+              ),
+              // CupertinoSearchTextField
+              child: CupertinoSearchTextField(
+                controller: _textEditingController,
+                onChanged: _onSearchChanged,
+                onSubmitted: _onSearchSubmitted,
+                autocorrect: false,
+              ),
             ),
             bottom: TabBar(
               controller: _tabController,
@@ -110,76 +121,81 @@ class _DiscoverScreenState extends State<DiscoverScreen>
                 padding: const EdgeInsets.all(
                   Sizes.size10,
                 ),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: width > Breakpoints.lg ? 5 : 2,
                   crossAxisSpacing: Sizes.size10,
                   mainAxisSpacing: Sizes.size10,
                   childAspectRatio: 9 / 20,
                 ),
-                itemBuilder: (context, index) => Column(
-                  children: [
-                    Container(
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(Sizes.size5),
-                      ),
-                      // AspectRatio: 이미지 비율대로 공간 조정해주는 위젯
-                      child: AspectRatio(
-                        aspectRatio: 9 / 16,
-                        // FadeInImage.assetNetwork : 이미지 로딩 시 placeholder사진 미리 보여주기
-                        child: FadeInImage.assetNetwork(
-                          // Boxfit : Image 사이즈 조정
-                          fit: BoxFit.cover,
-                          placeholder: "assets/images/placeholder.jpeg",
-                          image:
-                              "https://source.unsplash.com/random/200x${355 + index}",
+                // LayoutBuilder : 위젯의 최대 크기를 알려주는 위젯(부모의 크기)
+                itemBuilder: (context, index) => LayoutBuilder(
+                  builder: (context, constraints) => Column(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Sizes.size5),
+                        ),
+                        // AspectRatio: 이미지 비율대로 공간 조정해주는 위젯
+                        child: AspectRatio(
+                          aspectRatio: 9 / 16,
+                          // FadeInImage.assetNetwork : 이미지 로딩 시 placeholder사진 미리 보여주기
+                          child: FadeInImage.assetNetwork(
+                            // Boxfit : Image 사이즈 조정
+                            fit: BoxFit.cover,
+                            placeholder: "assets/images/placeholder.jpeg",
+                            image:
+                                "https://source.unsplash.com/random/200x${355 + index}",
+                          ),
                         ),
                       ),
-                    ),
-                    Gaps.v10,
-                    const Text(
-                      'This is a very long caption for my tiktok that im upload just now currently.',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: Sizes.size16,
-                        fontWeight: FontWeight.bold,
+                      Gaps.v10,
+                      const Text(
+                        'This is a very long caption for my tiktok that im upload just now currently.',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: Sizes.size16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Gaps.v8,
-                    // DefaultTextStyle 설정
-                    DefaultTextStyle(
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      child: Row(
-                        children: [
-                          const CircleAvatar(
-                            radius: 12,
-                            backgroundImage: NetworkImage(
-                                "https://lh3.googleusercontent.com/a/AGNmyxYol5lNtQShTuXHxFwtUaHFG7SJ7NgONKeSCEz9jg=s96-c-rg-br100"),
-                          ),
-                          Gaps.h4,
-                          const Expanded(
-                            child: Text(
-                              "My avatar is going to be very long",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          Gaps.h4,
-                          FaIcon(
-                            FontAwesomeIcons.heart,
-                            size: Sizes.size12,
+                      Gaps.v8,
+                      if (constraints.maxWidth < 200 ||
+                          constraints.maxWidth > 250)
+                        // DefaultTextStyle 설정
+                        DefaultTextStyle(
+                          style: TextStyle(
                             color: Colors.grey.shade600,
+                            fontWeight: FontWeight.w600,
                           ),
-                          Gaps.h2,
-                          const Text("2.5M")
-                        ],
-                      ),
-                    )
-                  ],
+                          child: Row(
+                            children: [
+                              const CircleAvatar(
+                                radius: 12,
+                                backgroundImage: NetworkImage(
+                                    "https://lh3.googleusercontent.com/a/AGNmyxYol5lNtQShTuXHxFwtUaHFG7SJ7NgONKeSCEz9jg=s96-c-rg-br100"),
+                              ),
+                              Gaps.h4,
+                              const Expanded(
+                                child: Text(
+                                  "My avatar is going to be very long",
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              Gaps.h4,
+                              FaIcon(
+                                FontAwesomeIcons.heart,
+                                size: Sizes.size12,
+                                color: Colors.grey.shade600,
+                              ),
+                              Gaps.h2,
+                              const Text("2.5M")
+                            ],
+                          ),
+                        )
+                    ],
+                  ),
                 ),
               ),
               for (var tab in tabs.skip(1))
